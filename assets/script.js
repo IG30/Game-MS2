380411f496893546d3160727ca4a7d3665fd3f0d
IG30 <Nacho.sanmor@gmail.com>
2020-09-10 20:01:14 +0000

    Add Sortable to the elements. Shufflebasketballs for loop is not working

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,51 @@
-
-
-
 class nameOfTheGame {
-    constructor(basketballs){
+  constructor(basketballs) {
     this.basketballsArray = basketballs;
+  }
+
+  startGame() {
+    this.shufflebasketballs();
+  }
+
+  shufflebasketballs() {
+    for (let i = this.basketballsArray.length - 1; i > 0; i--) {
+      let randomIndex = Math.floor(Math.random() * (i + 1));
+      console.log(randomIndex);
+      this.basketballsArray[randomIndex].style.order = i;
+      //this.basketballsArray[i].style.order = randomIndex;
     }
-
-    startGame(){
-        this.shufflebasketballs();
-    }
-
-
-    shufflebasketballs () {
-        for(let i = this.basketballsArray.length -1; i > 0; i--){
-            let randomIndex = Math.floor(Math.random() * (i+1));
-            this.basketballsArray[randomIndex].style.order = i;
-            this.basketballsArray[i].style.order = randomIndex;
-        }
-    }
-
-    
+  }
 }
 
-function ready(){
-    let overlays = Array.from(document.getElementsByClassName("overlay-text"));
-    let basketballs = Array.from(document.getElementsByClassName("basketballs"));
-    let game = new nameOfTheGame(basketballs);
 
-    overlays.forEach( overlay => {
-        overlay.addEventListener('click', () =>{
-            overlay.classList.remove('visible');
 
-            //code to start game go here
+function ready() {
+  let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+  let basketballs = Array.from(document.getElementsByClassName("basketballs"));
+  let game = new nameOfTheGame(basketballs);
 
-        });
-        
+  new Sortable(leftSide, {
+    group: 'shared', // set both lists to same group
+    animation: 150
     });
 
+    new Sortable(rightSide, {
+        group: 'shared',
+        animation: 150
+    });
+
+    new Sortable(ballsGroup, {
+    group: 'shared', // set both lists to same group
+    animation: 150
+    });
+
+  overlays.forEach((overlay) => {
+    overlay.addEventListener("click", () => {
+        overlay.classList.remove("visible");
+        //code to start game go here
+        game.startGame();
+    });
+  });
 }
 
+ready();
